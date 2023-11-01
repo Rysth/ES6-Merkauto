84f060565ab2f38edc5f7f66f2cb4df30c91f544
Rysth <johnpalacios.t@gmail.com>
2023-11-01 18:36:36 -0500

    Create customer information form

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import BrandLogo from './assets/images/brand.png';
 const headerContent = document.querySelector('#header');
 const brandImage = headerContent.querySelector('#brandImage');
 brandImage.src = BrandLogo;
+
+/* Main */
+const actualDate = new Date();
+const formatedDate = `${actualDate.getDay()}/${actualDate.getMonth()}/${actualDate.getFullYear()}`;
+document.querySelector('#actualDate').innerHTML = formatedDate;
